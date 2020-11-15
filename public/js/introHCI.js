614f62c58164e3cb130de9611f7b94c36721fb27
AE_CassA_Wei <alaricxw@gmail.com>
2020-11-14 17:20:31 -0800

    update

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,35 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("http://localhost:3000/project/"+idNumber, callBackFn(projectID));
 }
+
+
+// function addProjectDetails(e) {
+// 	// Prevent following the link
+// 	e.preventDefault();
+
+// 	// Get the div ID, e.g., "project3"
+// 	var projectID = $(this).closest('.project').attr('id');
+// 	// get rid of 'project' from the front of the id 'project3'
+// 	var idNumber = projectID.substr('project'.length);
+//     var temp = "https://localhost:3000/project/" + idNumber;
+    
+//     $.get(temp, function(data){
+
+//         var projectHTML = '<p>' + data['title'] + '</p>' + '<p>' + data['date'] + '</p>' + '<img src="' + data['image'] + '" class="detailsImage">' + '<p>' + data['summary'] + '</p>';
+
+//         $('#'+projectID+' .details').html(projectHTML);
+//     });
+// }
+
+
+function callBackFn(projectID) {
+	console.log("show")
+
+	var projectHTML = '<p>' + response['title'] + '</p>' + '<p>' + response['date'] + '</p>' + '<img src="' + response['image'] + '" class="detailsImage">' + '<p>' + response['summary'] + '</p>';
+
+	$('#'+projectID+' .details').html(projectHTML);
+}
+
+
